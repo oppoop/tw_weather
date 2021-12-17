8b64d9ae36982ee83f2c56d601eeb4123f7355dd
LAPTOP-382717MG\d0493 <d0350315@gmail.com>
2021-12-18 01:30:02 +0800

    設定頁面 init

---
+++ lib/app/modules/setting/views/setting_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/setting_controller.dart';
+
+class SettingView extends GetView<SettingController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      color: Colors.red,
+    );
+  }
+}
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import 'package:tw_weather/app/modules/home/bindings/home_binding.dart';
 import 'package:tw_weather/app/modules/home/views/home_view.dart';
+import 'package:tw_weather/app/modules/setting/bindings/setting_binding.dart';
+import 'package:tw_weather/app/modules/setting/views/setting_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SETTING,
+      page: () => SettingView(),
+      binding: SettingBinding(),
     ),
   ];
 }
