d6d86a5595650b07c04044e33cb7fe6511af81b5
LAPTOP-382717MG\d0493 <d0350315@gmail.com>
2021-12-20 18:14:51 +0800

    搜尋頁面UI

--- lib/app/modules/search/controllers/search_controller.dart
+++ lib/app/modules/search/controllers/search_controller.dart
@@ -1,20 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class SearchController extends GetxController {
-  //TODO: Implement SearchController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
+  final TextEditingController textEditingController = TextEditingController();
+  final FocusNode focusNode = FocusNode();
 }
--- lib/app/modules/search/views/search_view.dart
+++ lib/app/modules/search/views/search_view.dart
@@ -1,23 +1,149 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
+import '../../../constant.dart';
 import '../controllers/search_controller.dart';
 
 class SearchView extends GetView<SearchController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SearchView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'SearchView is working',
-          style: TextStyle(fontSize: 20),
+    return GetBuilder<SearchController>(
+        init: SearchController(),
+        builder: (controller) {
+          return Container(
+              height: Get.height,
+              width: Get.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    alignment: Alignment.bottomRight,
+                    image: AssetImage('assets/image/search_back.png')),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Align(
+                      child: Text(
+                        'Pick a location',
+                        style: TextStyle(
+                            color: kWhiteColor,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: Get.width * 0.7,
+                      child: Text(
+                        'Type the area or city you want to know the detailed weather information at this time.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: kWhiteColor, fontSize: 15),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    searchBlock(),
+                    featureCity(),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
+              ));
+        });
+  }
+
+  Widget searchBlock() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          height: 65,
+          width: Get.width * 0.6,
+          decoration: BoxDecoration(
+              color: CardColor, borderRadius: BorderRadius.circular(20)),
+          child: TextFormField(
+            style: TextStyle(color: Colors.white),
+            controller: controller.textEditingController,
+            focusNode: controller.focusNode,
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              hintText: 'Search',
+              hintStyle: TextStyle(color: kWhiteColor),
+              border: InputBorder.none,
+              suffixIcon: Icon(
+                Icons.search,
+                color: kWhiteColor,
+              ),
+            ),
+          ).paddingOnly(left: 20),
         ),
-      ),
+        const SizedBox(
+          width: 20,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 65,
+            width: Get.width * 0.15,
+            decoration: BoxDecoration(
+                color: CardColor, borderRadius: BorderRadius.circular(20)),
+            child: Icon(
+              Icons.location_on,
+              color: kWhiteColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget featureCity() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Container(
+              height: 150,
+              width: 120,
+              decoration: BoxDecoration(
+                  color: selectColor, borderRadius: BorderRadius.circular(15)),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 150,
+              width: 120,
+              decoration: BoxDecoration(
+                  color: CardColor, borderRadius: BorderRadius.circular(15)),
+            ),
+          ],
+        ),
+        const SizedBox(
+          width: 30,
+        ),
+        Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Container(
+              height: 300,
+              width: 120,
+              decoration: BoxDecoration(
+                  color: CardColor, borderRadius: BorderRadius.circular(15)),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
